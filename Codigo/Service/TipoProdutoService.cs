dea08398e27b99fc70ec8247d6f3685db4d776cf
Nadianne Galvão <54487179+Nadianne@users.noreply.github.com>
2023-09-08 13:43:15 -0300

    Adicionado sumário #117

--- Codigo/Service/TipoProdutoService.cs
+++ Codigo/Service/TipoProdutoService.cs
@@ -10,15 +10,22 @@ namespace Service
         {
             this.context = context;
         }
-
-
+        /// <summary>
+        /// Funcao para criar um tipoProdutoService
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public int Create(Tipoproduto tipoProduto)
         {
             context.Add(tipoProduto);
             context.SaveChanges();
             return tipoProduto.Id;
         }
-
+        /// <summary>
+        /// Funcao para Deleta um tipoProdutoService
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public void Delete(int idTipoProduto)
         {
             var tipoProduto = context.Tipoprodutos.Find(idTipoProduto);
@@ -26,24 +33,40 @@ namespace Service
             context.SaveChanges();
           
         }
-
+        /// <summary>
+        /// Funcao para Editar um tipoProdutoService
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public void Edit(Tipoproduto tipoProduto)
         {
             context.Update(tipoProduto);
             context.SaveChanges();
         }
-
+        /// <summary>
+        /// Funcao para procurar um tipoProdutoService
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public Tipoproduto Get(int tipoProduto)
         {
            return context.Tipoprodutos.Find(tipoProduto);
            
         }
-
+        /// <summary>
+        /// Funcao para retornar todos os tipoProdutoService
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public IEnumerable<Tipoproduto> GetAll()
         {
            return context.Tipoprodutos.AsNoTracking();
         }
-
+        /// <summary>
+        /// Funcao para consultar os tipoProdutoService pelo nome
+        /// </summary>
+        /// <param name="tipoProduto"></param>
+        /// <returns></returns>
         public IEnumerable<Tipoproduto> GetByNome(string nome)
         {
             var query = from tipoProduto in context.Tipoprodutos
