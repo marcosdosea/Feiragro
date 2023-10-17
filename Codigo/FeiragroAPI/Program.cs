5822234646b864c05e47dd85df2823c9c5ae356c
Ikaruz <58690965+Ikaruzdelta@users.noreply.github.com>
2023-10-16 22:35:34 -0300

    API implementada para produtos. Closed #187

--- Codigo/FeiragroAPI/Program.cs
+++ Codigo/FeiragroAPI/Program.cs
@@ -20,6 +20,7 @@ namespace FeiragroAPI
 
             builder.Services.AddTransient<IPessoaService, PessoaService>();
             builder.Services.AddTransient<IAssociacaoService, AssociacaoService>();
+            builder.Services.AddTransient<IProdutoService, ProdutoService>();
             builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             builder.Services.AddDbContext<FeiragroContext>(
